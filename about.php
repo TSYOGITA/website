ad91bafc5bb49dabf07538ce52c3a28e4e22fc02
TSYOGITA <77011543+TSYOGITA@users.noreply.github.com>
2023-06-21 10:59:38 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>PHP Website</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand bg-white font-weight-bolder" style="color: red;" href="#">BoTx</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/phpwebfull/index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/phpwebfull/about.php">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Information
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="services.php">SERVICES</a></li>
+            <li><a class="dropdown-item" href="contact.php">CONTACT</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<h1 class="jumbotron text-center">BotX Automations Pvt Ltd</h1>
+<h2 class="bg-info">WELCOME TO OUR WEBSITE</h2>
+<img src="https://th.bing.com/th/id/OIP.gi-1PBSxks_pUAtTVRsN2QHaE8?w=286&h=189&c=7&o=5&dpr=1.25&pid=1.7" alt="image">
+<br>
+<h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem<br> Ipsum has been the industry's standard dummy text ever since the 1500s, when an<br> unknown printer took a gallery</h6>
+<h6>Technology is advancing faster and taking less time to be widely adopted than ever<br> before.Things evolve,humans accept it and move on,It's time <br>to run with the changing world.It's time to be <br>the change.Botx Automations Private Limited is a startup <br>recognized by the Government of India.We deal in<br> the research,design,manufacturing,production and sales of<br> products in the field of robotics and automations</h6>
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
